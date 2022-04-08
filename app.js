11964fa6b7607dbdd976dbe28945d89c27e61ec8
Fernando Costa <62177135+nandoProgrammer@users.noreply.github.com>
2022-04-07 22:08:32 -0300

    add files

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-const express = require("express");
-const server = express();
+import "dotenv/config";
 
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+import express from "express";
+import * as Sentry from "@sentry/node";
+import Youch from "youch";
+import "express-async-errors";
 
-const auth = require('./src/middlewares/auth')
+import routes from "./routes";
 
-const user = [
-    {
-        user:"user1",
-        password: "12345"
-    }
-];
+import "./database";
 
-const books = [];
+import sentryConfig from "./config/sentry";
 
-server.use(bodyParser.json());
+class App {
+  constructor() {
+    this.server = express();
 
-server.post('/auth', (req, res) => {
-    if(req.body.user === user[0].user && req.body.password === user[0].password){
-       const token = jwt.sign({
-            user: user[0].user,
-            password: user[0].password
-       },
-       process.env.JWT_KEY,
-       {
-         expiresIn: '1h'
-       })
-       return res.status(200).json({
-           status:'sucess',
-           token: token
-       })
-    }else{
-       return res.status(401).json({ error: 'Not Found'})
-    }
-})
+    Sentry.init(sentryConfig);
 
-server.get('/books', auth, (req, res, next) => {
-    return res.status(200).json(books);
-})
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
 
+  middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: false }));
+  }
 
+  routes() {
+    this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
 
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === "development") {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
 
-server.listen(3000)
+      return res.status(500).json({ error: "Internal server error" });
+    });
+  }
+}
+
+export default new App().server;
